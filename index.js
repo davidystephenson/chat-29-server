8870e1582d983ed453dc78ca6c52901ee274ac53
David Y. Stephenson <david@davidystephenson.com>
2019-09-13 10:41:36 +0200

    Initiliase stream

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const Sse = require('json-sse')
+const factory = require('./router')
+
+const stream = new Sse()
 
 const app = express()
+
+const router = factory(stream)
+app.use(router)
 
 const port = process.env.PORT || 4000
 
