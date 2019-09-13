8e9dc1b367a1986d444b9bd96f985e149becb80e
David Y. Stephenson <david@davidystephenson.com>
2019-09-13 12:27:52 +0200

    Connected to database

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const express = require('express')
 const Sse = require('json-sse')
 
+const db = require('./db')
 const factory = require('./router')
 
 const stream = new Sse()
