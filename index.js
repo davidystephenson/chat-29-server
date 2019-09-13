9242ad01df0c9827bb9669e9ee62c5c6833bf6eb
David Y. Stephenson <david@davidystephenson.com>
2019-09-13 11:56:41 +0200

    Add CORS

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const express = require('express')
 const Sse = require('json-sse')
-const bodyParser = require('body-parser')
+
 const factory = require('./router')
 
 const stream = new Sse()
 
 const app = express()
+
+const middleware = cors()
+app.use(middleware)
 
 const jsonParser = bodyParser.json()
 app.use(jsonParser)
